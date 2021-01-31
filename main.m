7546041d0390e9536512ffacf2596613d4b217f4
linkel1 <linkel1@uw.edu>
2021-01-31 14:18:11 -0800

    Top-level template

---
+++ main.m
@@ -0,0 +1,21 @@
+clear all; close all; clc;
+
+%%
+Ts = 0; % Sampling Rate
+Tb = 0; % Baud/Symbol Rate
+SNR = 0; % Signal to Noise Ratio
+Lt = 0; % Total time for simulation
+Ksig = 0; % Number of signal instances
+
+% Generate input signals (Raised Cosine or Sinc)
+% Uniformly sampled within Lt
+% Generate sampled plot (INI)
+
+% Transmitter + Channel Noise (AWGN)
+% Generate input and input+noise plots
+% Normalize transmitted output
+
+
+% Receiver
+% Generate input+noise plot and Matched filter + thresholding
+
