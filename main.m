739d63fcf370f2162c8ca6f88314a016d6bc8f7b
Ali Alansari <afaa97@uw.edu>
2021-02-01 14:57:55 -0800

    Update main.m

--- main.m
+++ main.m
@@ -18,15 +18,7 @@ tsym = tsim(1:Tsym:end);    % Symbol timestamps
 % puls = sincpuls(Tsym, -5*Tsym:5*Tsym);
 puls = rtrcpuls(0.3, Tsym, -5*Tsym:5*Tsym);
 
-% Raised Cosine Pulse
-function y = rcpulse(a,t)
 
-    tau = 1; % Symbol time
-    t = t+0.0000001; % Insert offset to prevent NANs
-    tpi = pi/tau; atpi = a*tpi; at = 4*a^2/tau^2;
-    y = sin(tpi*t).*cos(atpi*t)./(tpi*t.*(1-at*t.^2));
-    
-end
 %% Generate pulse train
 % Uniformly sample (without replacement) from symbol timestamps
 ds = datasample(tsym(1:end-10), Ksig, 'Replace', false); % get symbol timestamps randomly
@@ -40,23 +32,7 @@ snr = 10^(SNRdB/10); snrA = sqrt(snr); % Compute SNR
 %% Transmitter + Channel Noise (AWGN)
 % Generate input and input+noise plots
 % Normalize transmitted output
-function snoise = transmitter(SNRdB, Ksig, Lt, s)
-    
-     snr = 10^(SNRdB/10);
-%     sigma2n = 1; % Unit variance for noise
-%     sigmaN = sqrt(sigma2N); % Standard Deviation of the noise
-    
-    snoise = s * snr + randn(Lt,1); % add noise to input signal
-    t = [1:Lt]'; % time index
-    
-    subplot(2,1,1) 
-    plot(t,s), title('Transmitted Signal W/O Noise')
-    
-    sublot(2,1,2)
-    plot(t,snoise), title('Noisy(AWGN) Signal')
-    
 
-end
 
 %% Receiver
 % Generate input+noise plot and Matched filter + thresholding
