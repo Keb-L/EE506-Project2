822f770fad701c418c844a94e514007b7fed5e75
Ali Alansari <afaa97@uw.edu>
2021-01-31 16:59:58 -0800

    Update main.m

--- main.m
+++ main.m
@@ -26,7 +26,23 @@ end
 %% Transmitter + Channel Noise (AWGN)
 % Generate input and input+noise plots
 % Normalize transmitted output
+function snoise = transmitter(SNRdB, Ksig, Lt, s)
+    
+     snr = 10^(SNRdB/10);
+%     sigma2n = 1; % Unit variance for noise
+%     sigmaN = sqrt(sigma2N); % Standard Deviation of the noise
+    
+    snoise = s * snr + randn(Lt,1); % add noise to input signal
+    t = [1:Lt]'; % time index
+    
+    subplot(2,1,1) 
+    plot(t,s), title('Transmitted Signal W/O Noise')
+    
+    sublot(2,1,2)
+    plot(t,snoise), title('Noisy(AWGN) Signal')
+    
 
+end
 
 %% Receiver
 % Generate input+noise plot and Matched filter + thresholding
