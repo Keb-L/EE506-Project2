86a091b7c9328c8d3059f130197e60297b80885b
Ali Alansari <afaa97@uw.edu>
2021-02-01 15:21:00 -0800

    Update rcpulse.m

--- rcpulse.m
+++ rcpulse.m
@@ -1,9 +1,9 @@
 % Raised Cosine Pulse
-function y = rcpulse(a,t)
+function y = rcpulse(a,Tsym, t)
 
-    tau = 1; % Symbol time
+    %tau = 1; % Symbol time
     t = t+0.0000001; % Insert offset to prevent NANs
-    tpi = pi/tau; atpi = a*tpi; at = 4*a^2/tau^2;
+    tpi = pi/Tsym; atpi = a*tpi; at = 4*a^2/Tsym^2;
     y = sin(tpi*t).*cos(atpi*t)./(tpi*t.*(1-at*t.^2));
     
 end
