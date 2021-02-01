edc1a8c5d6db9b221eea1df1cc78d457b5f9278a
Ali Alansari <afaa97@uw.edu>
2021-01-31 16:38:54 -0800

    Update main.m

--- main.m
+++ main.m
@@ -11,6 +11,15 @@ Ksig = 0; % Number of signal instances
 % Uniformly sampled within Lt
 % Generate sampled plot (INI)
 
+
+function y = rcpulse(a,t)
+
+    tau = 1; % Symbol time
+    t = t+0.0000001; % Insert offset to prevent NANs
+    tpi = pi/tau; atpi = a*tpi; at = 4*a^2/tau^2;
+    y = sin(tpi*t).*cos(atpi*t)./(tpi*t.*(1-at*t.^2));
+    
+end
 %% Generate pulse train
 
 
