1afbcdebd55740c822e15e4643bf39d0dd4877ef
Ali Alansari <afaa97@uw.edu>
2021-02-01 20:11:41 -0800

    Update transmitter.m

--- transmitter.m
+++ transmitter.m
@@ -4,27 +4,27 @@ function snoise = transmitter(SNR, Ksig, Lt, s,tsim)
 %     sigma2n = 1; % Unit variance for noise
 %     sigmaN = sqrt(sigma2N); % Standard Deviation of the noise
     
-    Ls = length(s);
-    Tsig = 1+floor((Ls-Lt).*rand(Ksig,1));
-    Tsig = sort(Tsig, 'ascend');
-    Tsig = unique(Tsig);
-    Ksig = length(Tsig);
-    %Tmarks = Ls-1+Tsig;
-    
-    hsig = zeros(Lt+1,1);
-    hsig(Tsig,:) = ones(Ksig,1); %These are the delay for each signal (or replica)
- 
-    sall = filter(s,1,hsig);%the filter delays and superposes ieach copy
+%     Ls = length(s);
+%     Tsig = 1+floor((Ls-Lt).*rand(Ksig,1));
+%     Tsig = sort(Tsig, 'ascend');
+%     Tsig = unique(Tsig);
+%     Ksig = length(Tsig);
+%     %Tmarks = Ls-1+Tsig;
+%     
+%     hsig = zeros(Lt+1,1);
+%     hsig(Tsig,:) = ones(Ksig,1); %These are the delay for each signal (or replica)
+%  
+%     sall = filter(s,1,hsig);%the filter delays and superposes ieach copy
 
     
     
     
-    snoise = sall * SNR + randn(Lt+1,1); % add noise to input signal
+    snoise = s * SNR + randn(Lt+1,1); % add noise to input signal
     %t = [1:Lt]'; % time index
     
     figure
     subplot(2,1,1) 
-    plot(tsim,sall), title('Transmitted Signal W/O Noise'), xlabel('Time')
+    plot(tsim,s), title('Transmitted Signal W/O Noise'), xlabel('Time')
     ylabel('s(t)')
     grid on
     
